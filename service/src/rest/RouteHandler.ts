5d1160bb4905c2a87ac4116dfcab304c083014e4
Nick Bradley <nickbradley@live.ca>
2017-02-19 16:51:00 -0800

    post logging

--- service/src/rest/RouteHandler.ts
+++ service/src/rest/RouteHandler.ts
@@ -31,7 +31,10 @@ export default class RouteHandler {
 
   public static async postSnippet(req: restify.Request, res: restify.Response, next: restify.Next) {
       Log.trace("Server::postSnippet(...) - id: " + JSON.stringify(req.params.id));
+
+      console.log("typeof req.body", typeof req.body);
       console.log("req.body", req.body);
+
       console.log("req.body.hash", req.body.hash);
       console.log("req.body.story", req.body.story);
       try {
